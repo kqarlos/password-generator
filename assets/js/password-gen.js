560f2098a6cd60626b6c313c1d074713944bb5da
kqarlos <kqarlos@hotmail.com>
2019-11-30 14:15:30 -0800

    Creates js file with variables and functions

---
+++ assets/js/password-gen.js
@@ -0,0 +1,17 @@
+
+var passwordPointer = document.getElementById("password");
+var length;
+var hasScpecialChars;
+var hasNumericChars;
+var haslowerCase;
+var hasUpperCase;
+
+function generatePassword(){
+
+
+}
+
+function copyToClipboard(){
+
+    
+}
