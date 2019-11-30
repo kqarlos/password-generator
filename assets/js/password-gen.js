768ffc84a382b760e4d2a745680c6048aa4b9b28
kqarlos <kqarlos@hotmail.com>
2019-11-30 14:58:15 -0800

    Button calls on function. Function generates a password based on user's desired length gotten from prompt

--- assets/js/password-gen.js
+++ assets/js/password-gen.js
@@ -5,13 +5,25 @@ var hasScpecialChars;
 var hasNumericChars;
 var haslowerCase;
 var hasUpperCase;
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=";
 
-function generatePassword(){
+function generatePassword() {
 
+    var newPassword = "";
+    do {
+        length = parseInt(prompt("How many characters"), 10);
+    } while (isNaN(length) || length < 8 || length > 128)
+
+    for (var i = 0; i < length; i++){
+        newPassword += characters[Math.floor(Math.random() * characters.length)];
+    }
+
+    
+    passwordPointer.innerHTML = newPassword;
 
 }
 
-function copyToClipboard(){
+function copyToClipboard() {
 
-    
+
 }
