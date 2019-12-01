d347bed399c2b3360a32e6d07e038d6df3f5fe70
kqarlos <kqarlos@hotmail.com>
2019-11-30 17:40:33 -0800

    Characters split into array types and accessed through a nested array. Adds confirm prompt for different character types.

--- assets/js/password-gen.js
+++ assets/js/password-gen.js
@@ -1,11 +1,15 @@
 
 var passwordPointer = document.getElementById("password");
 var length;
-var hasScpecialChars;
+var hasSpecialChars;
 var hasNumericChars;
-var haslowerCase;
+var hasLowerCase;
 var hasUpperCase;
-var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=";
+var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChars = "0123456789";
+var specialChars = "!@#$%^&*()-=";
+var characters = [lowerCaseChars, upperCaseChars, numericChars, specialChars];
 
 function generatePassword() {
 
@@ -14,13 +18,20 @@ function generatePassword() {
         length = parseInt(prompt("How many characters"), 10);
     } while (isNaN(length) || length < 8 || length > 128)
 
-    for (var i = 0; i < length; i++){
-        newPassword += characters[Math.floor(Math.random() * characters.length)];
+    hasNumericChars = confirm("Would you like numeric characters?")
+    hasSpecialChars = confirm("Would you like special characters?");
+    hasLowerCase = confirm("Would you like lower case characters?");
+    hasUpperCase = confirm("Would you like upper case characters?");
+
+    // console.log("Length: " + length + ", Numerics: " + hasNumericChars + ", Special: " + hasSpecialChars + ", Lower Case: " + hasLowerCase + ", Upper case: " + hasUpperCase);
+
+    for (var i = 0; i < length; i++) {
+        var charArray = Math.floor(Math.random() * characters.length);
+        // console.log(charArray);
+        newPassword += characters[charArray][Math.floor(Math.random() * characters[charArray].length)];
     }
 
-    
     passwordPointer.innerHTML = newPassword;
-
 }
 
 function copyToClipboard() {
